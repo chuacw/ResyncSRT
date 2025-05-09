fab2e2f2e1827a96f5f9125fd0e93dfde07028dc
chuacw <1757930+chuacw@users.noreply.github.com>
2025-05-09 14:42:58 +0800

    Initial check-in
    Initial check-in

--- FMXResyncSRTMain.pas
+++ FMXResyncSRTMain.pas
@@ -1,17 +1,20 @@
 // chuacw 2 Apr 2018
-unit ResyncSRTMain;
+unit FMXResyncSRTMain;
 
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Actions, Vcl.ActnList, Vcl.Menus,
-  Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.CheckLst;
+  System.Classes, FMX.Forms, System.Actions, FMX.ActnList, FMX.Dialogs, FMX.Controls,
+  FMX.Menus, FMX.ScrollBox, FMX.Memo, FMX.StdCtrls, FMX.ExtCtrls.Vcl, FMX.Types,
+  FMX.Controls.Presentation, FMX.Edit, FMX.DateTimeCtrls, System.Rtti,
+  FMX.Grid.Style, FMX.Grid, FMX.Layouts;
 
 type
   TfrmResync = class(TForm)
     Panel1: TPanel;
+    Button1: TButton;
     Panel2: TPanel;
+    Splitter1: TSplitter;
     memSRT: TMemo;
     memNewSRT: TMemo;
     MainMenu1: TMainMenu;
@@ -19,30 +22,19 @@ type
     Load1: TMenuItem;
     Save1: TMenuItem;
     N1: TMenuItem;
-    Exit1: TMenuItem;
+    MenuItem8: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
     ActionList1: TActionList;
     acLoad: TAction;
     acSave: TAction;
     acExit: TAction;
-    OpenDialog1: TOpenDialog;
-    Splitter1: TSplitter;
     acResync: TAction;
-    Button1: TButton;
     acTest: TAction;
-    leAdjustByHour: TLabeledEdit;
-    leAdjustByMin: TLabeledEdit;
-    leAdjustBySec: TLabeledEdit;
     rgSync: TRadioGroup;
-    SaveDialog1: TSaveDialog;
-    Label1: TLabel;
-    edNewLineNum: TEdit;
-    Label2: TLabel;
-    leStartFromHour: TLabeledEdit;
-    leStartFromMin: TLabeledEdit;
-    leStartFromSec: TLabeledEdit;
-    Panel3: TPanel;
-    Label3: TLabel;
-    Label4: TLabel;
+    leHour: TLabeledEdit;
+    leMin: TLabeledEdit;
+    leSec: TLabeledEdit;
     procedure acLoadExecute(Sender: TObject);
     procedure acResyncExecute(Sender: TObject);
     procedure acSaveExecute(Sender: TObject);
@@ -60,9 +52,9 @@ var
 
 implementation
 uses
-  System.DateUtils;
+  System.DateUtils, System.SysUtils;
 
-{$R *.dfm}
+{$R *.fmx}
 
 procedure TfrmResync.acExitExecute(Sender: TObject);
 begin
@@ -80,7 +72,7 @@ end;
 procedure TfrmResync.acResyncExecute(Sender: TObject);
 var
   I, J, LLineNum, LNewLineNum, LHour1, LMin1, LSec1, LMSec1, LHour2,
-  LMin2, LSec2, LMSec2, LShiftHour, LShiftMin, LShiftSec, LShiftMS: Word;
+  LMin2, LSec2, LMSec2, LShiftHour, LShiftMin, LShiftSec: Integer;
   LLine, LTimeInfo, LNewTimeInfo: string;
   LLines: TArray<string>;
   LTimeStart, LTimeEnd, LTimeShift: TDateTime;
@@ -90,30 +82,15 @@ begin
     memNewSRT.Lines.Clear;
 
     I := 0; LNewLineNum := 1;
-    var LStartHour := StrToInt(leStartFromHour.Text);
-    var LStartMin := StrToInt(leStartFromMin.Text);
-    var LStartSec := StrToInt(leStartFromSec.Text);
-
-    var LAdjustHour := StrToInt(leAdjustByHour.Text);
-    var LAdjustMin  := StrToInt(leAdjustByMin.Text);
-    var LAdjustSec  := StrToInt(leAdjustBySec.Text);
-
-    var LStartTime  := EncodeTime(LStartHour, LStartMin, LStartSec, 0);
-    var LAdjustTime := EncodeTime(LAdjustHour, LAdjustMin, LAdjustSec, 0);
-    var LDeltaTime  := Abs(LAdjustTime - LStartTime);
-
-    if LAdjustTime > LStartTime then
-      rgSync.ItemIndex := 0 else
-      rgSync.ItemIndex := 1;
-
-    DecodeTime(LDeltaTime, LShiftHour, LShiftMin, LShiftSec, LShiftMS);
+    LShiftHour := StrToInt(leHour.Text);
+    LShiftMin  := StrToInt(leMin.Text);
+    LShiftSec  := StrToInt(leSec.Text);
 
     while I < memSRT.Lines.Count-1 do
       begin
         SetLength(LLines, 0);
         LLineNum := StrToInt(memSRT.Lines[I]);
         Inc(I);
-
         // time info   00:00:00,349 --> 00:00:01,509
         LTimeInfo := memSRT.Lines[I];
         LLine     := LTimeInfo;
@@ -146,49 +123,31 @@ begin
         if Length(LLines) = 0 then
           Continue;
 
-        if edNewLineNum.Text<>'' then
-          begin
-            J := StrToInt(edNewLineNum.Text);
-            if LLineNum < J then Continue;
-          end;
-
-        // time info   00:00:00,349 --> 00:00:01,509
-        // First time is the time before the arrow
-        var LFirstTime := EncodeTime(LHour1, LMin1, LSec1, LMSec1);
-
-        // If this particular date/time is before the start time, skip
-        var LChangeTime := LFirstTime >= LStartTime;
-
         // start transposing
         memNewSRT.Lines.Add(IntToStr(LNewLineNum));
         Inc(LNewLineNum);
 
-        if LNewLineNum = 123 then
-          asm nop end;
+//      Redo...
+        case rgSync.ItemIndex of
+          0: begin // forward
+            LTimeStart := IncHour(LTimeStart,   LShiftHour);
+            LTimeStart := IncMinute(LTimeStart, LShiftMin);
+            LTimeStart := IncSecond(LTimeStart, LShiftSec);
 
-        if LChangeTime then
-          begin
-            case rgSync.ItemIndex of
-              0: begin // forward
-                LTimeStart := IncHour(LTimeStart,   LShiftHour);
-                LTimeStart := IncMinute(LTimeStart, LShiftMin);
-                LTimeStart := IncSecond(LTimeStart, LShiftSec);
-
-                LTimeEnd   := IncHour(LTimeEnd,     LShiftHour);
-                LTimeEnd   := IncMinute(LTimeEnd,   LShiftMin);
-                LTimeEnd   := IncSecond(LTimeEnd,   LShiftSec);
-              end;
-              1: begin // backward
-                LTimeStart := IncHour(LTimeStart,   -LShiftHour);
-                LTimeStart := IncMinute(LTimeStart, -LShiftMin);
-                LTimeStart := IncSecond(LTimeStart, -LShiftSec);
-
-                LTimeEnd   := IncHour(LTimeEnd,     -LShiftHour);
-                LTimeEnd   := IncMinute(LTimeEnd,   -LShiftMin);
-                LTimeEnd   := IncSecond(LTimeEnd,   -LShiftSec);
-              end;
-            end;
+            LTimeEnd   := IncHour(LTimeEnd,     LShiftHour);
+            LTimeEnd   := IncMinute(LTimeEnd,   LShiftMin);
+            LTimeEnd   := IncSecond(LTimeEnd,   LShiftSec);
           end;
+          1: begin // backward
+            LTimeStart := IncHour(LTimeStart,   -LShiftHour);
+            LTimeStart := IncMinute(LTimeStart, -LShiftMin);
+            LTimeStart := IncSecond(LTimeStart, -LShiftSec);
+
+            LTimeEnd   := IncHour(LTimeEnd,     -LShiftHour);
+            LTimeEnd   := IncMinute(LTimeEnd,   -LShiftMin);
+            LTimeEnd   := IncSecond(LTimeEnd,   -LShiftSec);
+          end;
+        end;
 
         LNewTimeInfo := Format('%s --> %s', [FormatDateTime('hh:nn:ss,zzz', LTimeStart),
           FormatDateTime('hh:nn:ss,zzz', LTimeEnd)]);
@@ -215,9 +174,7 @@ end;
 
 procedure TfrmResync.LoadFile(const AFileName: string);
 begin
-  memSRT.Lines.Clear;
   memSRT.Lines.LoadFromFile(AFileName);
-  memNewSRT.Lines.Clear;
 end;
 
 procedure TfrmResync.SaveFile(const AFileName: string);
--- ResyncSRTMain.dfm
+++ ResyncSRTMain.dfm
@@ -2,8 +2,8 @@ object frmResync: TfrmResync
   Left = 0
   Top = 0
   Caption = 'Resync Subtitles'
-  ClientHeight = 427
-  ClientWidth = 718
+  ClientHeight = 361
+  ClientWidth = 786
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,88 +12,185 @@ object frmResync: TfrmResync
   Font.Style = []
   Menu = MainMenu1
   OldCreateOrder = False
+  Position = poScreenCenter
   PixelsPerInch = 96
   TextHeight = 13
   object Panel1: TPanel
     Left = 0
     Top = 0
-    Width = 718
-    Height = 43
+    Width = 786
+    Height = 89
     Align = alTop
     TabOrder = 0
+    DesignSize = (
+      786
+      89)
+    object Label1: TLabel
+      Left = 576
+      Top = 14
+      Width = 49
+      Height = 13
+      Caption = 'Renumber'
+    end
+    object Label2: TLabel
+      Left = 671
+      Top = 14
+      Width = 19
+      Height = 13
+      Caption = 'to 1'
+    end
     object Button1: TButton
-      Left = 616
+      Left = 711
       Top = 9
-      Width = 75
+      Width = 66
       Height = 25
       Action = acResync
+      Anchors = [akRight, akBottom]
       TabOrder = 0
     end
-    object leHour: TLabeledEdit
-      Left = 360
+    object edNewLineNum: TEdit
+      Left = 632
       Top = 11
-      Width = 25
+      Width = 33
       Height = 21
-      EditLabel.Width = 23
-      EditLabel.Height = 13
-      EditLabel.Caption = 'Hour'
-      LabelPosition = lpLeft
       TabOrder = 1
-      Text = '0'
     end
-    object leMin: TLabeledEdit
-      Left = 408
-      Top = 11
-      Width = 25
-      Height = 21
-      EditLabel.Width = 16
-      EditLabel.Height = 13
-      EditLabel.Caption = 'Min'
-      LabelPosition = lpLeft
+    object Panel3: TPanel
+      Left = 9
+      Top = 40
+      Width = 768
+      Height = 41
+      BevelOuter = bvNone
       TabOrder = 2
-      Text = '0'
-    end
-    object leSec: TLabeledEdit
-      Left = 455
-      Top = 11
-      Width = 25
-      Height = 21
-      EditLabel.Width = 17
-      EditLabel.Height = 13
-      EditLabel.Caption = 'Sec'
-      LabelPosition = lpLeft
-      TabOrder = 3
-      Text = '0'
+      object Label3: TLabel
+        Left = 2
+        Top = 11
+        Width = 51
+        Height = 13
+        Hint = ' (skips hangling time before this)'
+        Caption = 'Start From'
+      end
+      object Label4: TLabel
+        Left = 304
+        Top = 8
+        Width = 44
+        Height = 13
+        Caption = 'Adjust to'
+      end
+      object leStartFromHour: TLabeledEdit
+        Left = 85
+        Top = 11
+        Width = 25
+        Height = 21
+        EditLabel.AlignWithMargins = True
+        EditLabel.Width = 23
+        EditLabel.Height = 13
+        EditLabel.Cursor = crCross
+        EditLabel.BiDiMode = bdRightToLeft
+        EditLabel.Caption = 'Hour'
+        EditLabel.Color = clHighlight
+        EditLabel.DragCursor = crMultiDrag
+        EditLabel.ParentBiDiMode = False
+        EditLabel.ParentColor = False
+        LabelPosition = lpLeft
+        TabOrder = 0
+        Text = '0'
+      end
+      object leStartFromMin: TLabeledEdit
+        Left = 133
+        Top = 11
+        Width = 25
+        Height = 21
+        EditLabel.Width = 16
+        EditLabel.Height = 13
+        EditLabel.Caption = 'Min'
+        LabelPosition = lpLeft
+        TabOrder = 1
+        Text = '0'
+      end
+      object leStartFromSec: TLabeledEdit
+        Left = 188
+        Top = 11
+        Width = 25
+        Height = 21
+        EditLabel.Width = 17
+        EditLabel.Height = 13
+        EditLabel.Caption = 'Sec'
+        LabelPosition = lpLeft
+        TabOrder = 2
+        Text = '0'
+      end
+      object leAdjustByHour: TLabeledEdit
+        Left = 394
+        Top = 11
+        Width = 25
+        Height = 21
+        EditLabel.AlignWithMargins = True
+        EditLabel.Width = 23
+        EditLabel.Height = 13
+        EditLabel.Cursor = crCross
+        EditLabel.BiDiMode = bdRightToLeft
+        EditLabel.Caption = 'Hour'
+        EditLabel.Color = clHighlight
+        EditLabel.DragCursor = crMultiDrag
+        EditLabel.ParentBiDiMode = False
+        EditLabel.ParentColor = False
+        LabelPosition = lpLeft
+        TabOrder = 3
+        Text = '0'
+      end
+      object leAdjustByMin: TLabeledEdit
+        Left = 442
+        Top = 11
+        Width = 25
+        Height = 21
+        EditLabel.Width = 16
+        EditLabel.Height = 13
+        EditLabel.Caption = 'Min'
+        LabelPosition = lpLeft
+        TabOrder = 4
+        Text = '0'
+      end
+      object leAdjustBySec: TLabeledEdit
+        Left = 497
+        Top = 11
+        Width = 25
+        Height = 21
+        EditLabel.Width = 17
+        EditLabel.Height = 13
+        EditLabel.Caption = 'Sec'
+        LabelPosition = lpLeft
+        TabOrder = 5
+        Text = '0'
+      end
     end
     object rgSync: TRadioGroup
       Left = 9
-      Top = -2
-      Width = 312
+      Top = -3
+      Width = 144
       Height = 40
-      Caption = 'Sync'
+      Caption = 'Sync Direction'
       Columns = 2
       Items.Strings = (
         'Forward'
         'Backward')
-      TabOrder = 4
+      TabOrder = 3
     end
   end
   object Panel2: TPanel
     Left = 0
-    Top = 43
-    Width = 718
-    Height = 384
+    Top = 89
+    Width = 786
+    Height = 272
     Align = alClient
     Caption = 'Panel2'
     TabOrder = 1
-    ExplicitLeft = 160
-    ExplicitTop = 96
-    ExplicitWidth = 185
-    ExplicitHeight = 41
+    ExplicitTop = 43
+    ExplicitHeight = 318
     object Splitter1: TSplitter
       Left = 361
       Top = 1
-      Height = 382
+      Height = 270
       ExplicitLeft = 360
       ExplicitTop = 208
       ExplicitHeight = 100
@@ -102,25 +199,27 @@ object frmResync: TfrmResync
       Left = 1
       Top = 1
       Width = 360
-      Height = 382
+      Height = 270
       Align = alLeft
       Lines.Strings = (
         'memSRT')
       ReadOnly = True
       ScrollBars = ssBoth
       TabOrder = 0
+      ExplicitHeight = 316
     end
     object memNewSRT: TMemo
       Left = 364
       Top = 1
-      Width = 353
-      Height = 382
+      Width = 421
+      Height = 270
       Align = alClient
       Lines.Strings = (
         'memNewSRT')
       ReadOnly = True
       ScrollBars = ssBoth
       TabOrder = 1
+      ExplicitHeight = 316
     end
   end
   object MainMenu1: TMainMenu
